57563ec9c94a022d5b115c17a92446dc1c016d86
Reynis Heremi Vazquez-Guzman <reynis@Reyniss-MacBook-Air.local>
2015-07-18 02:04:46 -0700

    added python webapp

---
+++ pinkthink/pinkthink.py
@@ -0,0 +1,41 @@
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            #self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+            #self.response.write('Hello, ' + user.nickname())
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user' : user,
+            'url' : url,
+            'url_linktext' : url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
